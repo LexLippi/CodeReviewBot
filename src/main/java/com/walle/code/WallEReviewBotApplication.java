0b8d620d6027481d28df755a4e29e4000c41bda1
lexlippi <lipatkin.a@brightfit.ru>
2021-09-24 21:43:47 +0500

    Create Spring Boot Application with Maven

---
+++ src/main/java/com/walle/code/WallEReviewBotApplication.java
@@ -0,0 +1,13 @@
+package com.walle.code;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WallEReviewBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WallEReviewBotApplication.class, args);
+    }
+
+}
